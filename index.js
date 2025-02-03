c552872b0697756ec29ef91de2028ebaed190b1b
Awopeju Kolawole Moses <Awopejumoses@gmail.com>
2025-02-03 12:13:15 +0100

    My HNG Stage 0 task

---
+++ index.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+  
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// User Data
+const user = { 
+    email: "awopejumoses@gmail.com",
+    current_datetime: new Date().toISOString(), 
+    github_url: "<https://github.com/Zymoclassic/HNG12/tree/main/stage0task>"
+}
+
+// Routes
+app.get("/", (req, res) => {
+  res.json(user);
+});
+
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
